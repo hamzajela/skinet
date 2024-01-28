c379d9c594b7c34da50aa10886898acd8b9ad731
hamzajela <hamzajsidorela@gmail.com>
2024-01-28 18:58:21 +0100

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,16 +12,19 @@ namespace API.Controllers
     public class ProductsController: ControllerBase
     {
   
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context) => _context = context;
+        public ProductsController(IProductRepository repo) 
+        {
+            _repo = repo;
 
+        }
 
         [HttpGet]
         public async Task <ActionResult<List<Product>>> GetProducts()
         {
-            var products =await _context.Products.ToListAsync();
-            return products;
+            var products =await _repo.GetProductsAsync();
+            return Ok(products);
 
     
         } 
@@ -29,8 +33,21 @@ namespace API.Controllers
         
         public async Task <ActionResult<Product>> GetProduct( int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
+ 
+        } 
 
-        }   
+         [HttpGet("brands")] 
+         public async Task <ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+         {
+            return Ok(await _repo.GetProductBrandsAsync ());
+         }
+
+         
+         [HttpGet("types")] 
+         public async Task <ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+         {
+            return Ok(await _repo.GetProductTypesAsync ());
+         }
     }
 }
--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -4,11 +4,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Core.Entities
 {
-    public class Product
+    public class Product: BaseEntity
     {
        // [Key]->It tells Visual Studio that this attribute is primary key
-        public int Id { get; set; }
+       // public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
         
+        public ProductType ProductType { get; set; }
+       public int ProductTypeId { get; set; }
+    
+       public ProductBrand ProductBrand { get; set; }
+       public int ProductBrandId { get; set; }
+    
     }
+
 }
